e2879cefa1dda4e2c1df091b1cf6e9cbcf00b142
wyroska <bsandoval@ing.ucsc.cl>
2023-11-03 14:30:44 -0300

    avance3-11

--- PRUEBAS/BASTIAN-SANDOVAL/login/includes/header.php
+++ PRUEBAS/BASTIAN-SANDOVAL/login/includes/header.php
@@ -5,7 +5,6 @@
 	<title>Ejemplo de navegación</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="PRUEBAS\BASTIAN SANDOVAL\login\includes\edit.css">
 	<!-- Bootstrap  -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
@@ -15,7 +14,8 @@
 	<!-- jQuery -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-	
+	<!-- CSS --> 
+	<link rel="stylesheet" href="http://localhost/xampp/PRUEBAS\BASTIAN-SANDOVAL\login\includes\edit.css">
 
 	<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>	
 
