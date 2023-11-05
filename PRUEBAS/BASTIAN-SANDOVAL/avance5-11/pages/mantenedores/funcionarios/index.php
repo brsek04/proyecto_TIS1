209ef11dfab0eb262bb8dd6180718a62770a9231
wyroska <bsandoval@ing.ucsc.cl>
2023-11-05 00:52:00 -0300

    avance5-11
    ya el interfaz va tomando welo :p

---
+++ PRUEBAS/BASTIAN-SANDOVAL/avance5-11/pages/mantenedores/funcionarios/index.php
@@ -0,0 +1,128 @@
+<?php
+    include("database/connection.php");  // Incluye la conexión
+    include("database/auth.php");  // Comprueba si el usuario está logueado, sino lo redirige al login
+
+    $query = "SELECT funcionarios.*, establecimientos.establecimiento AS establecimiento, departamentos.departamento AS departamento
+    FROM funcionarios
+    LEFT JOIN establecimientos ON funcionarios.establecimiento_id = establecimientos.id
+    LEFT JOIN departamentos ON funcionarios.departamento_id = departamentos.id;
+    ";
+
+    $result = mysqli_query($connection, $query);
+?>
+
+<main class="container mt-5">
+    <div class="d-flex" id="wrapper">
+        <div class="bg-white" id="sidebar-wrapper">
+           <li class="nav-item row align-items-start">
+                        <a class="nav-link <?php echo ($pagina == 'home') ? 'active' : null ?>" aria-current="page" href="index.php?p=home"><img class="p-0 m-1" src="logo-inventrack.png" alt="" width="240"></a>
+                    </li>
+
+            <div class="list-group list-group-flush my-3">
+                <a href="index.php?p=home" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                        class="fas fa-tachometer-alt me-2"></i>Inicio</a>
+                <a href="index.php?p=equipos/index" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                        class="fas fa-project-diagram me-2"></i>Equipos</a>
+                <a href="index.php?p=users/index" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                        class="fa-solid fa-user me-2"></i>Usuarios</a>
+                <a href="index.php?p=mantenedores/funcionarios/index" class="list-group-item list-group-item-action bg-transparent second-text active fw-bold"><i
+                        class="fa-solid fa-people-group me-2"></i>Funcionarios</a>
+                <a href="index.php?p=mantenedores/index" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                        class="fa-solid fa-bars-progress me-2"></i>Productos</a>
+                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                        class="fas fa-calendar me-2"></i>Calendario</a>
+                <a href="pages/auth/actions/logout.php" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold"><i
+                        class="fas fa-power-off me-2"></i>Salir</a>
+            </div>
+        </div>
+        <div id="page-content-wrapper">
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
+                    <h2 class="fs-2 m-0">Funcionarios</h2>
+                </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle second-text fw-bold" href="index.php?p=auth/profile" id="navbarDropdown"
+                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-user me-2"></i> <?php echo $_SESSION['username'] ?? null ?>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Perfil</a></li>
+                                <li><a class="dropdown-item" href="#">Configuracion</a></li>
+                                <li><a class="dropdown-item" href="#">Salir</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+<main class="container mt-5">
+
+
+    <div class="card">
+        <div class="card-header">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="text-center">
+
+                </div>
+                <div>              
+                    <a class="btn btn-sm btn-primary" href="index.php?p=mantenedores/funcionarios/create" role="button">Agregar nuevo</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body table-responsive ">
+            <table class="table table-hover">
+                <thead class="">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Apellido</th>
+                        <th scope="col">Correo electronico</th>
+                        <th scope="col">Establecimiento</th>
+                        <th scope="col">Departamento</th>
+                        <th scope="col">Opciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($fila = mysqli_fetch_array($result)) : ?>
+                        <tr>
+                            <th scope="row"><?= $fila['id'] ?></th>
+                            
+                            <td><?= $fila['nombre'] ?></td>
+                            <td><?= $fila['apellido'] ?></td>
+                            <td><?= $fila['email'] ?></td>
+                            <td><?= $fila['establecimiento'] ?></td>
+                            <td><?= $fila['departamento'] ?></td>
+                            <td>
+
+
+                            
+                                <a href="index.php?p=mantenedores/funcionarios/edit&id=<?= $fila['id'] ?>" class="btn btn-sm btn-outline-warning">Editar</a>
+                                
+                                <a href="pages/mantenedores/funcionarios/actions/delete.php?id=<?= $fila['id'] ?>" class="btn btn-sm btn-outline-danger">Eliminar</a>
+                            </td>
+                        </tr>
+
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+
+
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+</main>
