b21814bd34c69d847716b18c778a80e1ab5b429c
wyroska <bsandoval@ing.ucsc.cl>
2023-11-15 22:41:58 -0300

    avance15-11

---
+++ PRUEBAS/BASTIAN-SANDOVAL/avance15-11/pages/mantenedores/comunas/actions/store.php
@@ -0,0 +1,24 @@
+<?php
+    include("../../../../database/connection.php");
+
+    $opcion = $_POST["opcion"];
+    $region = $_POST["region"];
+    
+
+    $query = "INSERT INTO comunas (comuna, region_id) VALUES ('$opcion', '$region');";
+    $result =  mysqli_query($connection, $query);
+
+  
+    if ($result) {
+        // Set a session variable to indicate successful deletion
+        session_start();
+        $_SESSION['addition_success'] = true;
+    }
+
+
+
+    header("Location: ../../../../index.php?p=mantenedores/comunas/index");
+?>
+
+
+
