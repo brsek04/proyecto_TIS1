706a673228d43c7405a7bb3633cb8c24113294bb
martinbarra <mbarra@ing.ucsc.cl>
2023-11-29 11:40:18 -0300

    envio de alerta

--- PRUEBAS/MARTIN BARRA/avance28-11/pages/calendario/alerta.php
+++ PRUEBAS/MARTIN BARRA/avance28-11/pages/calendario/alerta.php
@@ -3,42 +3,41 @@
 <?php 
 include("../../database/connection.php");
 
-  
-    $id = $_GET['id'];
+$id = $_GET['id'];
 
-    // Asumo que $id es el ID de la mantención
-    $query = "SELECT m.*, e.funcionario_id 
-              FROM mantenciones m 
-              JOIN equipos e ON m.equipo_id = e.id 
-              WHERE m.id = $id";
+// Asumo que $id es el ID de la mantención
+$query = "SELECT m.*, e.funcionario_id, e.id as idEquipo
+          FROM mantenciones m 
+          JOIN equipos e ON m.equipo_id = e.id 
+          WHERE m.id = $id";
 
-    $result = mysqli_query($connection, $query);
+$result = mysqli_query($connection, $query);
 
-    if ($result && $row = mysqli_fetch_assoc($result)) {
-        $funcionario_id = $row['funcionario_id'];
-        $fechaMantencion = $row['start_date'];
+if ($result && $row = mysqli_fetch_assoc($result)) {
+    $funcionario_id = $row['funcionario_id'];
+    $fechaMantencion = $row['start_date'];
+    $idEquipo = $row['idEquipo'];
 
-        // Obtener información del funcionario
-        $queryFuncionario = "SELECT * FROM funcionarios WHERE id = $funcionario_id";
-        $resultFuncionario = mysqli_query($connection, $queryFuncionario);
+    // Obtener información del funcionario
+    $queryFuncionario = "SELECT * FROM funcionarios WHERE id = $funcionario_id";
+    $resultFuncionario = mysqli_query($connection, $queryFuncionario);
 
-        if ($resultFuncionario && $rowFuncionario = mysqli_fetch_assoc($resultFuncionario)) {
-            $funcionario_nombre = $rowFuncionario['nombre'];
-            $funcionario_email = $rowFuncionario['email'];
+    if ($resultFuncionario && $rowFuncionario = mysqli_fetch_assoc($resultFuncionario)) {
+        $funcionario_nombre = $rowFuncionario['nombre'];
+        $funcionario_email = $rowFuncionario['email'];
 
-            // Construir la descripción del mensaje
-            $descripcion2 = "$funcionario_nombre, recuerde mantenimiento equipo #$id - $fechaMantencion";
+        // Construir la descripción del mensaje
+        $descripcion2 = "$funcionario_nombre, recuerde mantenimiento equipo #$idEquipo - $fechaMantencion";
 
-            // Imprimir los campos como inputs hidden
-            echo '<input type="hidden" id="sendername" value="inventrack">';
-            echo '<input type="hidden" id="to" value="' . $funcionario_email . '">';
-            echo '<input type="hidden" id="subject" value="Mantenimiento de Equipo">';
-            echo '<input type="hidden" id="replyto" value="">';
-            echo '<input type="hidden" id="message" value="' . $descripcion2 . '">';
+        // Imprimir los campos como inputs hidden
+        echo '<input type="hidden" id="sendername" value="inventrack">';
+        echo '<input type="hidden" id="to" value="' . $funcionario_email . '">';
+        echo '<input type="hidden" id="subject" value="Mantenimiento de Equipo">';
+        echo '<input type="hidden" id="replyto" value="">';
+        echo '<input type="hidden" id="message" value="' . $descripcion2 . '">';
 
-
-            session_start();
-            $_SESSION['mantencion_success'] = true;
+        session_start();
+        $_SESSION['mantencion_success'] = true;
 
         } else {
             // Manejar el caso en que no se pueda obtener información del funcionario
